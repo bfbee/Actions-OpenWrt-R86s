da271f463b6cf67d2f4d4c87dba654e6c4952624
bfbee <bfbee@outlook.com>
2023-03-19 17:05:53 +0800

    Add luci-app-smartdns

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+git clone https://github.com/pymumu/luci-app-smartdns -b lede package/lean/luci-app-smartdns
